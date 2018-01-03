366213ba79235ef4116f105cb224939398ea1a60
Leonardo de Souza <leonardo.souza@trigg.com.br>
2018-01-03 16:16:08 -0200

    Fix. deslize dos botões no mobile

--- app/components/gamepad.js
+++ app/components/gamepad.js
@@ -17,25 +17,20 @@ Gamepad = function(game) {
     this.createButtons = function(){
         // Esquerda
         this.buttons.left = game.add.button(30, game.height - 100, 'btn_left', null, this, 0, 0, 1, 0);
-        this.buttons.left.onInputDown.add(() => this.pressKey(this.buttons.left, this.keys.left), this);
-        this.buttons.left.onInputUp.add(() => this.keys.left.isDown = false, this);
+        this.buttons.left.onInputOver.add(() => this.keys.left.isDown = true, this);
+        this.buttons.left.onInputOut.add(() => this.keys.left.isDown = false, this);
 
         // Direita
         this.buttons.right = game.add.button(130, game.height - 100, 'btn_right', null, this, 0, 0, 1, 0);
-        this.buttons.right.onInputDown.add(() => this.pressKey(this.buttons.right, this.keys.right), this);
-        this.buttons.right.onInputUp.add(() => this.keys.right.isDown = false, this);
+        this.buttons.right.onInputOver.add(() => this.keys.right.isDown = true, this);
+        this.buttons.right.onInputOut.add(() => this.keys.right.isDown = false, this);
 
         // Cima
         this.buttons.up = game.add.button(game.width - 100, game.height - 100, 'btn_up', null, this, 0, 0, 1, 0);
-        this.buttons.up.onInputDown.add(() => this.keys.up.isDown = true, this);
-        this.buttons.up.onInputUp.add(() => this.keys.up.isDown = false, this);
+        this.buttons.up.onInputOver.add(() => this.keys.up.isDown = true, this);
+        this.buttons.up.onInputOut.add(() => this.keys.up.isDown = false, this);
 
         // Fixando botões na tela
         for (var button in this.buttons ) { this.buttons[button].fixedToCamera = true; }
-    }
-
-    this.pressKey = function(button, key){
-        key.isDown = true;
-        key.durationUp = button.input.overDuration();
     }
 }
