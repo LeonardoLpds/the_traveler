4ecb6317fdff8e73bc1d6da5007a4cea1f4f3dd6
Leonardo de Souza <leonardo.souza@trigg.com.br>
2018-01-04 14:27:43 -0200

    Criando pasta public (necessário para o firebase)

--- public/app/states/playGame.js
+++ public/app/states/playGame.js
@@ -40,6 +40,7 @@ playGame.prototype = {
         // verifica movimentos do player
         this.player.move(this.gamepad.keys);
 
+            this.map.clouds.x = (this.player.player.position.x) / 3;
     },
 
     addAudios: function(){
