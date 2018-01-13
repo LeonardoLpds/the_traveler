260b76045615e73addbe2c36615fbab7252ec7ff
Leonardo <leonardo.lpds@gmail.com>
2018-01-12 23:41:23 -0200

    Alterando antialias e scale

--- public/app/states/boot.js
+++ public/app/states/boot.js
@@ -6,7 +6,9 @@ boot.prototype = {
     },
     create: function () {
         this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.game.scale.refresh();
+        this.scale.setMinMax(1024/2, 672/2, 1024*2, 672*2);
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = false;
 
         this.game.state.start("Preload");
     }
